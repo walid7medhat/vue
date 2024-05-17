200e97462d69211226fb65bd7126a0ac4e29bdaf
walid7medhat <walid.medhat@icloud.com>
2024-05-17 18:58:49 +0400

    first push

---
+++ src/components/SignUp.vue
@@ -0,0 +1,16 @@
+<template>
+  <img class="logo" alt="Vue logo" src="../assets/logo.png">
+<h1>Sign Up</h1>
+</template>
+<script>
+export default {
+    name : 'SignUp'
+}
+</script>
+<style>
+.logo{
+    width: 100px;
+    height: 100px;
+    margin-top: 10px;
+    margin-bottom: 10px;
+}</style>
