81f64dbabda4566dd838521f7f3edbd61affa683
walid7medhat <walid.medhat@icloud.com>
2024-05-22 10:53:16 +0400

    signup

---
+++ src/components/Home.vue
@@ -0,0 +1,15 @@
+<template>
+<h1>hello user, welcome on home page</h1>
+</template>
+
+<script>
+export default { 
+    // eslint-disable-next-line vue/multi-word-component-names
+    name: 'Home',
+        mounted() {
+        if (!localStorage.getItem('user-info')) {
+            this.$router.push({ name: 'SignUp' });
+        }
+    }
+}
+</script>
--- src/components/SignUp.vue
+++ src/components/SignUp.vue
@@ -2,18 +2,54 @@
   <img class="logo" alt="Vue logo" src="../assets/logo.png">
 <h1>Sign Up</h1>
 <div class="register">
-<input type="text" placeholder="enter name">
-<input type="text" placeholder="enter Email">
-<input type="password" placeholder="enter Password">
-<button> Sign Up</button>
+<input type="text" v-model="name" placeholder="enter name">
+<input type="text" v-model="email" placeholder="enter Email">
+<input type="password" v-model="password" placeholder="enter Password">
+<button   v-on:click="signUp"> Sign Up</button>
 
 </div>
 </template>
 <script>
+import axios from 'axios';
+
 export default {
-    name : 'SignUp'
-}
+    name: 'SignUp',
+    data() { 
+        return {
+            name: '',
+            email: '',
+            password: ''
+        };
+    },
+    methods: {
+        async signUp() {
+            try {
+                let response = await axios.post('http://localhost:3000/users', {
+                    name: this.name,
+                    email: this.email,
+                    password: this.password
+                });
+                if (response.status === 201) {
+                    localStorage.setItem('user-info', JSON.stringify(response.data));
+                    this.$router.push({ name: 'Home' });
+                } else {
+                    console.log('Unexpected response status:', response.status);
+                }
+                console.log(response);
+            } catch (error) { 
+                console.error('Error during sign up:', error);
+            }
+        }
+    },
+    mounted() {
+        if (localStorage.getItem('user-info')) {
+            this.$router.push({ name: 'Home' });
+        }
+    }
+
+};
 </script>
+
 <style>
 .logo{
     width: 100px;
